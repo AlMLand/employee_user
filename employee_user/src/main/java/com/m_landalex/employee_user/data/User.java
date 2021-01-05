59c033e209c75c331f2087879e0f7d74ceec150e
AlMLand <morlandalex@googlemail.com>
2021-01-05 12:24:06 +0100

    Modified class user. Removed the list of employees, added the variable.

--- employee_user/src/main/java/com/m_landalex/employee_user/data/User.java
+++ employee_user/src/main/java/com/m_landalex/employee_user/data/User.java
@@ -1,7 +1,5 @@
 package com.m_landalex.employee_user.data;
 
-import java.util.List;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,14 +18,6 @@ public class User extends AbstractObject {
 	private String username;
 	private String password;
 	private Role userRole;
-	private List<User> userList;
+	private Employee employee;
 
-	public User(String username, String password, Role userRole) {
-		super();
-		this.username = username;
-		this.password = password;
-		this.userRole = userRole;
-		this.userList.add(this);
-	}
-	
 }
