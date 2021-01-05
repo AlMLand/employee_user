54f2a8770e0704f0137cd274ee17d8edfa8b6654
AlMLand <morlandalex@googlemail.com>
2021-01-05 21:28:25 +0100

    Modified the method toObjectList. Removed Collection and added the List.

--- employee_user/src/main/java/com/m_landalex/employee_user/mapper/Mapper.java
+++ employee_user/src/main/java/com/m_landalex/employee_user/mapper/Mapper.java
@@ -1,6 +1,6 @@
 package com.m_landalex.employee_user.mapper;
 
-import java.util.Collection;
+import java.util.List;
 
 import com.m_landalex.employee_user.data.AbstractObject;
 import com.m_landalex.employee_user.domain.AbstractEntity;
@@ -9,6 +9,6 @@ public interface Mapper<S extends AbstractEntity,D extends AbstractObject> {
 	
 	S toEntity(D object);
 	D toObject(S entity);
-	Collection<D> toObjectList(Collection<S> entityList);
+	List<D> toObjectList(List<S> entityList);
 
 }
