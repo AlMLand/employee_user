f019d9b266628ed61244a794c9397f6d61713f46
AlMLand <morlandalex@googlemail.com>
2021-01-05 21:32:27 +0100

    Modified the EmployeeRepository.

--- employee_user/src/main/java/com/m_landalex/employee_user/persistence/EmployeeRepository.java
+++ employee_user/src/main/java/com/m_landalex/employee_user/persistence/EmployeeRepository.java
@@ -5,10 +5,11 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.m_landalex.employee_user.data.Employee;
+import com.m_landalex.employee_user.domain.EmployeeEntity;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
-	List<Employee> findByFirstName(String firstName);
-	List<Employee> findByLastName(String lastName);
+	List<EmployeeEntity> findByFirstName(String firstName);
+	List<EmployeeEntity> findByLastName(String lastName);
 	
 }
