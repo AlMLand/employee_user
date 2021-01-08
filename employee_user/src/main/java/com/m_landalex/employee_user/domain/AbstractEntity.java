9b859cfe31c692abf9cd97e6cd885b73d9cb8058
AlMLand <morlandalex@googlemail.com>
2021-01-08 23:54:47 +0100

    Added the @Getter and @Setter annotation.

--- employee_user/src/main/java/com/m_landalex/employee_user/domain/AbstractEntity.java
+++ employee_user/src/main/java/com/m_landalex/employee_user/domain/AbstractEntity.java
@@ -5,6 +5,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 @MappedSuperclass
 public abstract class AbstractEntity {
 
