38093c244fe9b95cf99c5da0a6b3b60119891b2e
AlMLand <morlandalex@googlemail.com>
2021-03-03 11:32:23 +0100

    Deleted the Configurable context.

--- employee_user/src/main/java/com/m_landalex/employee_user/view/DemoRunFile.java
+++ employee_user/src/main/java/com/m_landalex/employee_user/view/DemoRunFile.java
@@ -8,7 +8,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.m_landalex.employee_user.data.Employee;
@@ -34,9 +33,7 @@ public class DemoRunFile implements CommandLineRunner {
 
 	public static void main(String[] args) throws IOException {
 		
-		ConfigurableApplicationContext context = SpringApplication.run(DemoRunFile.class, args);
-		System.in.read();
-		context.close();
+		SpringApplication.run(DemoRunFile.class, args);
 		
 	}
 
