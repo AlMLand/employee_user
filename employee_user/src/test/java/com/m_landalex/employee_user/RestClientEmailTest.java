e6a02a530d065ba0cbdd76815ed16fc39a4b7f43
AlMLand <morlandalex@googlemail.com>
2021-03-05 11:15:51 +0100

    Create the class rest test email.

---
+++ employee_user/src/test/java/com/m_landalex/employee_user/RestClientEmailTest.java
@@ -0,0 +1,81 @@
+package com.m_landalex.employee_user;
+
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.web.client.RestTemplate;
+
+import com.m_landalex.employee_user.data.Email;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@TestMethodOrder(OrderAnnotation.class)
+public class RestClientEmailTest {
+
+	private RestTemplate restTemplate;
+	private static final String URL_CREATE_EMAIL = "http://localhost:8080/emails/";
+	private static final String URL_GET_ALL_EMAILS = "http://localhost:8080/emails/";
+	private static final String URL_PUT_EMAIL_BY_ID = "http://localhost:8080/emails/{id}";
+	private static final String URL_GET_EMAIL_BY_ID = "http://localhost:8080/emails/{id}";
+	private static final String URL_DELETE_EMAIL_BY_ID = "http://localhost:8080/emails/{id}";
+	
+	@BeforeEach
+	public void setUp() {
+		restTemplate = new RestTemplate();
+		log.info("Started test...");
+	}
+	
+	@AfterEach
+	public void tearDown() {
+		log.info("Ended test...");
+	}
+	
+	@Test
+	@Order(1)
+	public void testFetchAllEmails() {
+		Email[] returnedArray = restTemplate.getForObject(URL_GET_ALL_EMAILS, Email[].class);
+		assertEquals(2, returnedArray.length);
+	}
+	
+	@Test
+	@Order(2)
+	public void testFetchEmailById() {
+		Email returnedEmail = restTemplate.getForObject(URL_GET_EMAIL_BY_ID, Email.class, 1);
+		assertEquals("mcgregor@googlemail.com", returnedEmail.getEmail());
+	}
+	
+	@Test
+	@Order(3)
+	public void testUpdateEmailById() {
+		Email returnedEmail = restTemplate.getForObject(URL_GET_EMAIL_BY_ID, Email.class, 1);
+		returnedEmail.setEmail("test@gmail.com");
+		restTemplate.put(URL_PUT_EMAIL_BY_ID, returnedEmail, 1);
+		returnedEmail = restTemplate.getForObject(URL_GET_EMAIL_BY_ID, Email.class, 1);
+		assertEquals("test@gmail.com", returnedEmail.getEmail());
+	}
+	
+	@Test
+	@Order(4)
+	public void testCreateEmail() {
+		Email newEmail = Email.builder().email("testCreateEmail@gmail.com").build();
+		restTemplate.postForObject(URL_CREATE_EMAIL, newEmail, Email.class);
+		Email returnedEmail = restTemplate.getForObject(URL_GET_EMAIL_BY_ID, Email.class, 3);
+		assertEquals("testCreateEmail@gmail.com", returnedEmail.getEmail());
+	}
+	
+	@Test
+	@Order(5)
+	public void testDeleteStandingAloneEmailById() {
+		restTemplate.delete(URL_DELETE_EMAIL_BY_ID, 3);
+		Email[] returnedArray = restTemplate.getForObject(URL_GET_ALL_EMAILS, Email[].class);
+		assertEquals(2, returnedArray.length);
+	}
+	
+}
