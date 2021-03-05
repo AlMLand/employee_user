9caecb3cb9507d5b9f3a66b71a594b5ea6dce7d2
AlMLand <morlandalex@googlemail.com>
2021-03-05 11:15:01 +0100

    Modified methods.

--- employee_user/src/main/java/com/m_landalex/employee_user/service/EmailService.java
+++ employee_user/src/main/java/com/m_landalex/employee_user/service/EmailService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.m_landalex.employee_user.data.Email;
+import com.m_landalex.employee_user.domain.EmailEntity;
 import com.m_landalex.employee_user.mapper.EmailMapper;
 import com.m_landalex.employee_user.persistence.EmailRepository;
 
@@ -21,10 +22,26 @@ public class EmailService {
 		emailRepository.save(emailMapper.toEntity(email));
 		return email;
 	}
+	
+	public Email update(Email email) {
+		EmailEntity returnedEmailEntity = emailRepository.findById(email.getId()).get();
+		returnedEmailEntity.setEmail(emailMapper.toEntity(email).getEmail());
+		emailRepository.save(returnedEmailEntity);
+		return email;
+	}
 
 	@Transactional(readOnly = true)
 	public List<Email> fetchAll() {
 		return emailMapper.toObjectList(emailRepository.findAll());
 	}
+	
+	@Transactional(readOnly = true)
+	public Email fetchById(Long id) {
+		return emailMapper.toObject(emailRepository.findById(id).get());
+	}
 
+	public void deleteById(Long id) {
+		emailRepository.delete(emailRepository.findById(id).get());
+	}
+	
 }
