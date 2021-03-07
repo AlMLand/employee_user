6966f82cf6ea02e25a0693a2794aadb4c6863b15
AlMLand <morlandalex@googlemail.com>
2021-03-07 11:40:42 +0100

    Create the rabbitmq listener.

---
+++ employee_user/src/main/java/com/m_landalex/employee_user/service/EmployeeCreateListener.java
@@ -0,0 +1,23 @@
+package com.m_landalex.employee_user.service;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class EmployeeCreateListener {
+
+	@RabbitListener(queues = "queueNumberOne")
+	public void queue_1Listener(String statusMessage) {
+		if(statusMessage.equals("succesful")) {
+			log.info("ObJeCt Is SaVeD [ {} ]", statusMessage);
+		}else if (statusMessage.equals("error")) {
+			log.info("ObJeCt Is NoT SaVeD [ {} ]", statusMessage);
+		}else {
+			log.info("not option");
+		}
+	}
+	
+}
