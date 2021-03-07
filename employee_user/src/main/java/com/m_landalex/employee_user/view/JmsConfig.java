684389ea14b5e1312408a24d59da05a744e1d1d9
AlMLand <morlandalex@googlemail.com>
2021-03-07 11:39:59 +0100

    Removed the MadelMapper bean from Config.class, crated the ModelMapperConfig.class with ModelMapper bean. Unnamed the Config.class to the JmsConfig.class.

--- employee_user/src/main/java/com/m_landalex/employee_user/view/JmsConfig.java
+++ employee_user/src/main/java/com/m_landalex/employee_user/view/JmsConfig.java
@@ -2,9 +2,6 @@ package com.m_landalex.employee_user.view;
 
 import javax.jms.ConnectionFactory;
 
-import org.modelmapper.ModelMapper;
-import org.modelmapper.config.Configuration.AccessLevel;
-import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,15 +10,7 @@ import org.springframework.jms.config.JmsListenerContainerFactory;
 import org.springframework.jms.listener.DefaultMessageListenerContainer;
 
 @Configuration
-public class Config {
-
-	@Bean
-	public ModelMapper modelMapper() {
-		ModelMapper modelMapper = new ModelMapper();
-		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT).setFieldMatchingEnabled(true)
-				.setSkipNullEnabled(true).setFieldAccessLevel(AccessLevel.PRIVATE);
-		return modelMapper;
-	}
+public class JmsConfig {
 
 	/*
 	 * so that the JmsTemplate (when receiving) is not blocked
@@ -34,5 +23,5 @@ public class Config {
 		defaultJmsListenerContainerFactoryConfigurer.configure(containerFactory, connectionFactory);
 		return containerFactory;
 	}
-
+	
 }
