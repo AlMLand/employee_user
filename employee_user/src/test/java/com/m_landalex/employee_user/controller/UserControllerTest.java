6ec96065b6c916a6ef0e7c4287900979d835f954
AlMLand <morlandalex@googlemail.com>
2021-03-10 18:09:55 +0100

    Added the controller package to the test directory.

--- employee_user/src/test/java/com/m_landalex/employee_user/controller/UserControllerTest.java
+++ employee_user/src/test/java/com/m_landalex/employee_user/controller/UserControllerTest.java
@@ -1,4 +1,4 @@
-package com.m_landalex.employee_user;
+package com.m_landalex.employee_user.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
