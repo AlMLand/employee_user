015d715061384905ec8d888c39a791f23b7914e5
AlMLand <morlandalex@googlemail.com>
2021-03-17 13:21:12 +0100

    Added the annotation @EnableMBeanExport

--- employee_user/src/main/java/com/m_landalex/employee_user/view/DemoRunFile.java
+++ employee_user/src/main/java/com/m_landalex/employee_user/view/DemoRunFile.java
@@ -5,11 +5,13 @@ import java.io.IOException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.EnableMBeanExport;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = "com.m_landalex.employee_user")
 @EnableJpaRepositories(basePackages = "com.m_landalex.employee_user.persistence")
+@SpringBootApplication(scanBasePackages = "com.m_landalex.employee_user")
 @EntityScan("com.m_landalex.employee_user.domain")
+@EnableMBeanExport
 public class DemoRunFile {
 
 	public static void main(String[] args) throws IOException {
