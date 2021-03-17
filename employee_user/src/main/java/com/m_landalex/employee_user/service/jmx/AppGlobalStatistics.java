34d0a0eb4553824fe6c9a9475251461d69385cbb
AlMLand <morlandalex@googlemail.com>
2021-03-17 13:20:34 +0100

    Crate the class for jmx statistic

---
+++ employee_user/src/main/java/com/m_landalex/employee_user/service/jmx/AppGlobalStatistics.java
@@ -0,0 +1,47 @@
+package com.m_landalex.employee_user.service.jmx;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jmx.export.annotation.ManagedOperation;
+import org.springframework.jmx.export.annotation.ManagedResource;
+import org.springframework.stereotype.Component;
+
+import com.m_landalex.employee_user.service.AddressService;
+import com.m_landalex.employee_user.service.EmailService;
+import com.m_landalex.employee_user.service.EmployeeService;
+import com.m_landalex.employee_user.service.UserService;
+
+@Component
+@ManagedResource( description = "JMX managed ressource", objectName = "jmxDemo:name=App_employee_user" )
+public class AppGlobalStatistics {
+
+	@Autowired private EmployeeService employeeService;
+	@Autowired private AddressService addressService;
+	@Autowired private EmailService emailService;
+	@Autowired private UserService userService;
+	
+	@ManagedOperation( description = "Quantity of employees in the application" )
+	public long countAllEmployees() {
+		return employeeService.countAllEmployees();
+	}
+	
+	@ManagedOperation( description = "Quantity of users in the application" )
+	public long countAllUsers() {
+		return userService.countAllUsers();
+	}
+	
+	@ManagedOperation( description = "Quantity of addresses in the application" )
+	public long countAllAddresses() {
+		return addressService.countAllAddresses();
+	}
+	
+	@ManagedOperation( description = "Quantity of emails in the application" )
+	public long countAllEmails() {
+		return emailService.countAllEmails();
+	}
+	
+	@ManagedOperation( description = "Quantity of objects in the application" )
+	public long countAllObjects() {
+		return countAllEmployees() + countAllUsers() + countAllAddresses() + countAllEmails();
+	}
+	
+}
