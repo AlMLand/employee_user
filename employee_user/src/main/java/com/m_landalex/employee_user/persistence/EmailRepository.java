a97c683652a3ff4a37e6cadb62314b6a124b084a
AlMLand <morlandalex@googlemail.com>
2021-05-12 16:38:12 +0200

    Removed methods

--- employee_user/src/main/java/com/m_landalex/employee_user/persistence/EmailRepository.java
+++ employee_user/src/main/java/com/m_landalex/employee_user/persistence/EmailRepository.java
@@ -1,13 +1,9 @@
 package com.m_landalex.employee_user.persistence;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.m_landalex.employee_user.domain.EmailEntity;
 
 public interface EmailRepository extends JpaRepository<EmailEntity, Long> {
-
-	List<EmailEntity> findAll();
 
 }
