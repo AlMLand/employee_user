791db2e39567441196693c770413b0603258e98a
AlMLand <morlandalex@googlemail.com>
2021-05-12 19:55:07 +0200

    New data structure for setupInitialization()

--- employee_user/src/main/java/com/m_landalex/employee_user/service/dbinitialization/DBInitialization.java
+++ employee_user/src/main/java/com/m_landalex/employee_user/service/dbinitialization/DBInitialization.java
@@ -1,6 +1,7 @@
 package com.m_landalex.employee_user.service.dbinitialization;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
 
@@ -16,7 +17,7 @@ import com.m_landalex.employee_user.data.User;
 import com.m_landalex.employee_user.exception.AsyncXAResourcesException;
 import com.m_landalex.employee_user.service.EmployeeService;
 
-@Profile( "!test" )
+@Profile("!test")
 @Service
 public class DBInitialization {
 
@@ -26,19 +27,35 @@ public class DBInitialization {
 	@PostConstruct
 	public void setupInitialization() throws AsyncXAResourcesException {
 
-		employeeService.save(Employee.builder().firstName("Connor").lastName("McGregor").age(32)
-				.salary(new BigDecimal(5000.00d)).email(Email.builder().email("mcgregor@googlemail.com").build())
-				.addressData(Address.builder().street("Aaaa street").houseNumber(10).city("Dublin").postCode("12345")
-						.build())
-				.userData(User.builder().username("A_A_A").password("B_B_B").userRole(Role.DEVELOPMENT).build())
-				.build());
+		employeeService.save(
+				Employee.builder().firstName("Connor").lastName("McGregor").age(32).salary(new BigDecimal(5000.00d))
+				.email(
+						Email.builder().email("mcgregor@googlemail.com").build()
+						)
+				.addressData(
+						Address.builder().street("Aaaa street").houseNumber(10).city("Dublin").postCode("12345").build()
+						)
+				.userData(
+						User.builder().username("A_A_A").password("B_B_B")
+						.userRoles(List.of(Role.builder().role("ADMINISTRATOR").build())).build()
+						)
+				.build()
+				);
 
-		employeeService.save(Employee.builder().firstName("Dustin").lastName("Poirier").age(31)
-				.salary(new BigDecimal(4000.00d)).email(Email.builder().email("poirier@googlemail.com").build())
-				.addressData(Address.builder().street("Bbbb street").houseNumber(10).city("Atlanta").postCode("67890")
-						.build())
-				.userData(User.builder().username("C_C_C").password("D_D_D").userRole(Role.PROJECTMANAGEMENT).build())
-				.build());
+		employeeService.save(
+				Employee.builder().firstName("Dustin").lastName("Poirier").age(31).salary(new BigDecimal(4000.00d))
+				.email(
+						Email.builder().email("poirier@googlemail.com").build()
+						)
+				.addressData(
+						Address.builder().street("Bbbb street").houseNumber(10).city("Atlanta").postCode("67890").build()
+						)
+				.userData(
+						User.builder().username("C_C_C").password("D_D_D")
+						.userRoles(List.of(Role.builder().role("ADMINISTRATOR").build())).build()
+						)
+				.build()
+				);
 
 	}
 
