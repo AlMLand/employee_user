a710962a8e295f4a15bd56981c1ffd5b882c9d10
AlMLand <morlandalex@googlemail.com>
2021-05-12 20:02:33 +0200

    Modified the path to=/rest/.. ; removed updateEmailById() method

--- employee_user/src/main/java/com/m_landalex/employee_user/view/controller/EmailController.java
+++ employee_user/src/main/java/com/m_landalex/employee_user/view/controller/EmailController.java
@@ -2,51 +2,47 @@ package com.m_landalex.employee_user.view.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.m_landalex.employee_user.data.Email;
+import com.m_landalex.employee_user.exception.AsyncXAResourcesException;
 import com.m_landalex.employee_user.service.EmailService;
 
 @RestController
-@RequestMapping(value = "/emails")
+@RequestMapping(value = "/rest/emails")
 public class EmailController {
 
 	@Autowired
-	private EmailService emailService;
+	private EmailService service;
 	
 	@PostMapping(value = "/")
-	public Email createEmail(@RequestBody Email email) {
-		emailService.save(email);
+	public Email create(@Valid @RequestBody Email email) throws AsyncXAResourcesException {
+		service.save(email);
 		return email;
 	}
 	
 	@GetMapping(value = "/")
-	public List<Email> fetchAllEmails(){
-		return emailService.fetchAll();
-	}
-	
-	@PutMapping(value = "/{id}")
-	public Email updateEmailById(@PathVariable Long id, @RequestBody Email email ) {
-		emailService.update(email);
-		return email;
+	public List<Email> list(){
+		return service.fetchAll();
 	}
 	
 	@GetMapping(value = "/{id}")
-	public Email fetchEmailById(@PathVariable Long id) {
-		return emailService.fetchById(id);
+	public Email findById(@PathVariable Long id) {
+		return service.fetchById(id);
 	}
 	
 	@DeleteMapping(value = "/{id}")
-	public void deleteStandingAloneEmailById(@PathVariable Long id) {
-		emailService.deleteById(id);
+	public void deleteStandingAloneById(@PathVariable Long id) {
+		service.deleteById(id);
 	}
 	
 }
