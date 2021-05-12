af4f299f6bec79cdbd8cb029c086507b2e120d8b
AlMLand <morlandalex@googlemail.com>
2021-05-12 16:08:21 +0200

    Modified from enum to class

--- employee_user/src/main/java/com/m_landalex/employee_user/data/Role.java
+++ employee_user/src/main/java/com/m_landalex/employee_user/data/Role.java
@@ -1,7 +1,32 @@
 package com.m_landalex.employee_user.data;
 
-public enum Role {
+import static java.util.Comparator.*;
 
-	SUPPORT, ADMINISTRATION, DEVELOPMENT, PROJECTMANAGEMENT;
+import java.util.Comparator;
+
+import javax.validation.constraints.NotBlank;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class Role extends AbstractObject implements Comparable<Role>{
+
+	@NotBlank(message = "{javax.validation.constraints.NotBlank.message}")
+	private String role;
+
+	private static final Comparator<Role> COMPARATOR_ROLE = comparing(Role::getRole);
+	
+	@Override
+	public int compareTo(Role o) {
+		return COMPARATOR_ROLE.compare(this, o);
+	}
 
 }
