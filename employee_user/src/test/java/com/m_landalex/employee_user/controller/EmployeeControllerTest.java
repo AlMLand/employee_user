53b88aa918e1aee4841ea9500f17931c7dd6eaab
AlMLand <morlandalex@googlemail.com>
2021-05-12 20:27:20 +0200

    Modified tests adaptation to changes in the project

--- employee_user/src/test/java/com/m_landalex/employee_user/controller/EmployeeControllerTest.java
+++ employee_user/src/test/java/com/m_landalex/employee_user/controller/EmployeeControllerTest.java
@@ -1,8 +1,6 @@
 package com.m_landalex.employee_user.controller;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -26,7 +24,7 @@ import com.m_landalex.employee_user.data.Role;
 import com.m_landalex.employee_user.data.User;
 import com.m_landalex.employee_user.exception.AsyncXAResourcesException;
 import com.m_landalex.employee_user.service.EmployeeService;
-import com.m_landalex.employee_user.view.controller.EmployeeController;
+import com.m_landalex.employee_user.view.controller.rest.EmployeeController;
 
 @ExtendWith(MockitoExtension.class)
 public class EmployeeControllerTest {
@@ -40,23 +38,28 @@ public class EmployeeControllerTest {
 	public void setUp() {
 		listEmployees  = new ArrayList<>();
 		employeeController = new EmployeeController();
+		ReflectionTestUtils.setField(employeeController, "service", mockedEmployeeService);
 		Employee employee = Employee.builder().firstName("Test_firstName_1").lastName("Test_lastName_1").age(100)
 				.salary(new BigDecimal(5000.00d)).email(Email.builder().email("test_1@googlemail.com").build())
 				.addressData(Address.builder().street("Test_street_1").houseNumber(10).city("Test_city_1").postCode("12345")
 						.build())
-				.userData(User.builder().username("A_A_A").password("B_B_B").userRole(Role.DEVELOPMENT).build())
+				.userData(User.builder().username("A_A_A").password("B_B_B").userRoles(
+						List.of(Role.builder().role("DEVELOPMENT").build())
+						).build())
 				.build();
 		employee.setId(1L);
 		listEmployees.add(employee);
 	}
 	
 	@Test
-	public void createEmployeeTest() throws AsyncXAResourcesException {
+	public void createTest() throws AsyncXAResourcesException {
 		Employee newEmployee = Employee.builder().firstName("Test_firstName_2").lastName("Test_lastName_2").age(100)
 				.salary(new BigDecimal(5000.00d)).email(Email.builder().email("test_2@googlemail.com").build())
 				.addressData(Address.builder().street("Test_street_2").houseNumber(10).city("Test_city_2").postCode("12345")
 						.build())
-				.userData(User.builder().username("C_C_C").password("D_D_D").userRole(Role.DEVELOPMENT).build())
+				.userData(User.builder().username("C_C_C").password("D_D_D").userRoles(
+						List.of(Role.builder().role("DEVELOPMENT").build())
+						).build())
 				.build();
 		
 		Mockito.when(mockedEmployeeService.save(newEmployee)).thenAnswer(new Answer<Employee>() {
@@ -67,10 +70,9 @@ public class EmployeeControllerTest {
 				return newEmployee;
 			}
 		});
-		ReflectionTestUtils.setField(employeeController, "employeeService", mockedEmployeeService);
 		
 		ExtendedModelMap extendedModelMap = new ExtendedModelMap();
-		extendedModelMap.addAttribute("createEmployee", employeeController.createEmployee(newEmployee));
+		extendedModelMap.addAttribute("createEmployee", employeeController.create(newEmployee));
 		Employee returnedNewEmployee = (Employee) extendedModelMap.get("createEmployee");
 		
 		assertNotNull(listEmployees);
@@ -79,22 +81,20 @@ public class EmployeeControllerTest {
 	}
 	
 	@Test
-	public void createEmployeeShouldThrowRuntimeExceptiontest() throws AsyncXAResourcesException {
+	public void createShouldThrowRuntimeExceptiontest() throws AsyncXAResourcesException {
 		Employee newEmployee = null;
 		Mockito.when(mockedEmployeeService.save(newEmployee)).thenThrow(RuntimeException.class);
-		ReflectionTestUtils.setField(employeeController, "employeeService", mockedEmployeeService);
 		assertThrows(RuntimeException.class, ()->{
-			employeeController.createEmployee(newEmployee);
+			employeeController.create(newEmployee);
 		});
 	}
 	
 	@Test
-	public void fetchAllEmployeesTest() {
+	public void findAllTest() {
 		Mockito.when(mockedEmployeeService.fetchAll()).thenReturn(listEmployees);
-		ReflectionTestUtils.setField(employeeController, "employeeService", mockedEmployeeService);
 		
 		ExtendedModelMap extendedModelMap = new ExtendedModelMap();
-		extendedModelMap.addAttribute("fetchAllEmployees", employeeController.fetchAllEmployees());
+		extendedModelMap.addAttribute("fetchAllEmployees", employeeController.list());
 		@SuppressWarnings("unchecked")
 		List<Employee> returnedList = (List<Employee>) extendedModelMap.get("fetchAllEmployees");
 		
@@ -103,7 +103,7 @@ public class EmployeeControllerTest {
 	}
 	
 	@Test
-	public void deleteAllEmployeesTest() {
+	public void deleteAllTest() {
 		Mockito.doAnswer(new Answer<Employee>() {
 
 			@Override
@@ -112,21 +112,19 @@ public class EmployeeControllerTest {
 				return null;
 			}
 		}).when(mockedEmployeeService).deleteAll();
-		ReflectionTestUtils.setField(employeeController, "employeeService", mockedEmployeeService);
 		
-		employeeController.deleteAllEmployees();
+		employeeController.deleteAll();
 		
 		assertNotNull(listEmployees);
 		assertEquals(0, listEmployees.size());
 	}
 	
 	@Test
-	public void fetchEmployeeByIdTest() {
+	public void findByIdTest() {
 		Mockito.when(mockedEmployeeService.fetchById(Mockito.anyLong())).thenReturn(listEmployees.get(0));
-		ReflectionTestUtils.setField(employeeController, "employeeService", mockedEmployeeService);
 		
 		ExtendedModelMap extendedModelMap = new ExtendedModelMap();
-		extendedModelMap.addAttribute("fetchEmployeeById", employeeController.fetchEmployeeById(Mockito.anyLong()));
+		extendedModelMap.addAttribute("fetchEmployeeById", employeeController.findById(Mockito.anyLong()));
 		Employee returnedEmployee = (Employee) extendedModelMap.get("fetchEmployeeById");
 		
 		assertNotNull(returnedEmployee);
@@ -141,44 +139,7 @@ public class EmployeeControllerTest {
 	}
 	
 	@Test
-	public void updateEmployeeByIdTest() throws AsyncXAResourcesException {
-		Employee employee = Employee.builder().firstName("Test_firstName_2").lastName("Test_lastName_2").age(10)
-				.salary(new BigDecimal(1000.00d)).email(Email.builder().email("test_2@googlemail.com").build())
-				.addressData(Address.builder().street("Test_street_2").houseNumber(12).city("Test_city_2").postCode("56789")
-						.build())
-				.userData(User.builder().username("C_C_C").password("D_D_D").userRole(Role.ADMINISTRATION).build())
-				.build();
-		
-		Mockito.when(mockedEmployeeService.update(Mockito.any(Employee.class))).thenAnswer(new Answer<Employee>() {
-
-			@Override
-			public Employee answer(InvocationOnMock invocation) throws Throwable {
-				Employee returnedEmployee = listEmployees.get(0);
-				returnedEmployee.setFirstName(employee.getFirstName());
-				returnedEmployee.setLastName(employee.getLastName());
-				returnedEmployee.setAge(employee.getAge());
-				returnedEmployee.setAddressData(employee.getAddressData());
-				returnedEmployee.setSalary(employee.getSalary());
-				returnedEmployee.setEmail(employee.getEmail());
-				returnedEmployee.setUserData(employee.getUserData());
-				listEmployees.remove(0);
-				listEmployees.add(returnedEmployee);
-				return returnedEmployee;
-			}
-		});
-		ReflectionTestUtils.setField(employeeController, "employeeService", mockedEmployeeService);
-		
-		ExtendedModelMap extendedModelMap = new ExtendedModelMap();
-		extendedModelMap.addAttribute("epdateEmployeeById", employeeController.updateEmployeeById(1L, employee));
-		Employee updatedEmployee = (Employee) extendedModelMap.get("epdateEmployeeById");
-		
-		assertNotNull(updatedEmployee);
-		assertEquals(1, listEmployees.size());
-		assertEquals("Test_firstName_2", updatedEmployee.getFirstName());
-	}
-	
-	@Test
-	public void deleteEmployeeByIdTest() {
+	public void deleteByIdTest() {
 		Mockito.doAnswer(new Answer<Employee>() {
 
 			@Override
@@ -187,9 +148,8 @@ public class EmployeeControllerTest {
 				return null;
 			}
 		}).when(mockedEmployeeService).deleteById(Mockito.anyLong());
-		ReflectionTestUtils.setField(employeeController, "employeeService", mockedEmployeeService);
 		
-		employeeController.deleteEmployeeById(999L);
+		employeeController.deleteById(999L);
 		
 		assertNotNull(listEmployees);
 		assertEquals(0, listEmployees.size());
