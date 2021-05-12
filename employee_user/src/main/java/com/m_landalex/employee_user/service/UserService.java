f82d2dc89426bfc14b32e07fc7afa88df9ab374f
AlMLand <morlandalex@googlemail.com>
2021-05-12 19:57:34 +0200

    Added a rabbitmq and logger

--- employee_user/src/main/java/com/m_landalex/employee_user/service/UserService.java
+++ employee_user/src/main/java/com/m_landalex/employee_user/service/UserService.java
@@ -2,57 +2,57 @@ package com.m_landalex.employee_user.service;
 
 import java.util.List;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.m_landalex.employee_user.data.User;
-import com.m_landalex.employee_user.domain.UserEntity;
 import com.m_landalex.employee_user.exception.AsyncXAResourcesException;
 import com.m_landalex.employee_user.mapper.UserMapper;
 import com.m_landalex.employee_user.persistence.UserRepository;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Transactional
 @Service
 public class UserService {
 
-	@Autowired 	private UserMapper userMapper;
-	@Autowired 	private JmsTemplate jmsTemplate;
-	@Autowired 	private UserRepository userRepository;
+	@Autowired private UserMapper mapper;
+	@Autowired private JmsTemplate jmsTemplate;
+	@Autowired private UserRepository repository;
+	@Autowired private RabbitTemplate rabbitTemplate;
+	@Value("${rabbitmq.queueName}") private String queueName;
 
-	public User save( User user ) throws AsyncXAResourcesException {
-		if ( user == null ) {
-			throw new AsyncXAResourcesException( "Symulation going wrong" );
+	public User save(User user) throws AsyncXAResourcesException {
+		if (user == null) {
+			log.error("Error by save from user object");
+			rabbitTemplate.convertAndSend(queueName, "error by save from user object");
+			throw new AsyncXAResourcesException("User object is null -> method save");
 		}
-		userRepository.save( userMapper.toEntity( user ) );
-		jmsTemplate.convertAndSend( "users", "-->User with username " + user.getUsername() + " is saved " );
-		return user;
-	}
-	
-	public User update( User user ) {
-		UserEntity returnedUserEntity = userRepository.findById( user.getId() ).get();
-		returnedUserEntity.setUsername( userMapper.toEntity( user ).getUsername() );
-		returnedUserEntity.setPassword( userMapper.toEntity( user ).getPassword() );
-		returnedUserEntity.setUserRole( userMapper.toEntity( user ).getUserRole() );
-		userRepository.save(returnedUserEntity);
-		return user;
+		User newUser = mapper.toObject(repository.save(mapper.toEntity(user)));
+		rabbitTemplate.convertAndSend(queueName, "succesful by save from email object");
+		jmsTemplate.convertAndSend("users", "-->User with username " + user.getUsername() + " is saved ");
+		return newUser;
 	}
 
-	@Transactional( readOnly = true )
+	@Transactional(readOnly = true)
 	public List<User> fetchAll() {
-		return userMapper.toObjectList(userRepository.findAll());
+		return mapper.toObjectList(repository.findAll());
 	}
 
-	@Transactional( readOnly = true )
-	public User fetchById( Long id ) {
-		return userMapper.toObject(userRepository.findById(id).get());
+	@Transactional(readOnly = true)
+	public User fetchById(Long id) {
+		return mapper.toObject(repository.findById(id).get());
 	}
-	
-	@Transactional( propagation = Propagation.NEVER )
+
+	@Transactional(propagation = Propagation.NEVER)
 	public Long countAllUsers() {
-		return userRepository.count();
+		return repository.count();
 	}
 
 }
