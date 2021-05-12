6095d02550029016bd7070f82103767ce913f7b9
AlMLand <morlandalex@googlemail.com>
2021-05-12 19:59:09 +0200

    Added a rabbitmq and logger, removed update() method

--- employee_user/src/main/java/com/m_landalex/employee_user/service/EmailService.java
+++ employee_user/src/main/java/com/m_landalex/employee_user/service/EmailService.java
@@ -2,50 +2,58 @@ package com.m_landalex.employee_user.service;
 
 import java.util.List;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.m_landalex.employee_user.data.Email;
-import com.m_landalex.employee_user.domain.EmailEntity;
+import com.m_landalex.employee_user.exception.AsyncXAResourcesException;
 import com.m_landalex.employee_user.mapper.EmailMapper;
 import com.m_landalex.employee_user.persistence.EmailRepository;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Transactional
 @Service
 public class EmailService {
 
-	@Autowired 	private EmailRepository emailRepository;
-	@Autowired 	private EmailMapper emailMapper;
+	@Autowired private EmailMapper mapper;
+	@Autowired private EmailRepository repository;
+	@Autowired private RabbitTemplate rabbitTemplate;
+	@Value("${rabbitmq.queueName}") private String queueName;
 
-	public Email save(Email email) {
-		emailRepository.save(emailMapper.toEntity(email));
-		return email;
+	public Email save(Email email) throws AsyncXAResourcesException {
+		if(email == null) {
+			log.error("Error by save from email object");
+			rabbitTemplate.convertAndSend(queueName, "error by save from email object");
+			throw new AsyncXAResourcesException("Email object is null -> method save");
+		}
+		Email newEmail = mapper.toObject(repository.save(mapper.toEntity(email)));
+		rabbitTemplate.convertAndSend(queueName, "succesful by save from email object");
+		return newEmail;
 	}
 	
-	public Email update(Email email) {
-		EmailEntity returnedEmailEntity = emailRepository.findById(email.getId()).get();
-		returnedEmailEntity.setEmail(emailMapper.toEntity(email).getEmail());
-		emailRepository.save(returnedEmailEntity);
-		return email;
-	}
-
 	@Transactional(readOnly = true)
 	public List<Email> fetchAll() {
-		return emailMapper.toObjectList(emailRepository.findAll());
+		return mapper.toObjectList(repository.findAll());
 	}
 	
 	@Transactional(readOnly = true)
 	public Email fetchById(Long id) {
-		return emailMapper.toObject(emailRepository.findById(id).get());
+		return mapper.toObject(repository.findById(id).get());
 	}
 
 	public void deleteById(Long id) {
-		emailRepository.delete(emailRepository.findById(id).get());
+		repository.delete(repository.findById(id).get());
 	}
 	
+	@Transactional(propagation = Propagation.NEVER)
 	public long countAllEmails() {
-		return emailRepository.count();
+		return repository.count();
 	}
 	
 }
