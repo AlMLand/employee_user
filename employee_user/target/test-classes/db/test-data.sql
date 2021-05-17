17af29a6037d978f9584c91d65759a9bedea948a
AlMLand <morlandalex@googlemail.com>
2021-05-17 14:20:56 +0200

    Added the employee input value age

--- employee_user/target/test-classes/db/test-data.sql
+++ employee_user/target/test-classes/db/test-data.sql
@@ -10,8 +10,8 @@ values(1, 0, '12345', 'test_username');
 insert into roles (id, version, role) 
 values (1, 0, 'DEVELOPMENT');
 
-insert into employees (id, version, address_employee, birthdate, email_id, firstname, lastname, salary, user_id) 
-values(1, 0, 1, '1985-06-05', 1, 'test_firstname', 'test_lastname', 5000, 1);
+insert into employees (id, version, address_employee, birthdate, age, email_id, firstname, lastname, salary, user_id) 
+values(1, 0, 1, '1985-06-05', 35, 1, 'test_firstname', 'test_lastname', 5000, 1);
 
 insert into users_roles (user_fk, role_fk) 
 values (1, 1);
