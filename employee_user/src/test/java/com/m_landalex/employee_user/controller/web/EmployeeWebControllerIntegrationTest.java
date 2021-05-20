32274c749c6c41d8e7eb2b0ba979df938c108961
AlMLand <morlandalex@googlemail.com>
2021-05-20 16:53:07 +0200

    Added all tests to the class

--- employee_user/src/test/java/com/m_landalex/employee_user/controller/web/EmployeeWebControllerIntegrationTest.java
+++ employee_user/src/test/java/com/m_landalex/employee_user/controller/web/EmployeeWebControllerIntegrationTest.java
@@ -3,23 +3,15 @@ package com.m_landalex.employee_user.controller.web;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.timeout;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoInteractions;
-import static org.mockito.Mockito.when;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.hamcrest.Matchers;
@@ -28,6 +20,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -108,7 +103,7 @@ public class EmployeeWebControllerIntegrationTest {
 	@DisplayName("when HTTP status 302, then save employee, render view and HTTP 302 redirected")
 	@Test
 	public void save_Test2() throws JsonProcessingException, Exception {
-		when(service.save(any(Employee.class))).thenReturn(employee);
+		when(service.save(Mockito.any(Employee.class))).thenReturn(employee);
 		mockMvc.perform(post("/employees/")
 				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
 				.param("firstName", employee.getFirstName())
@@ -189,6 +184,7 @@ public class EmployeeWebControllerIntegrationTest {
 		.andExpect(model().attributeDoesNotExist("firstName", "lastName", "birthDate", "age", "salary", "email.email",
 				"addressData.street", "addressData.houseNumber", "addressData.city", "userData.username", "userData.password",
 				"userData.userRoles"));
+		verifyNoInteractions(service);
 	}
 	
 	@DisplayName("when not authorized user, then return status HTTP 401 - unauthorized, verifying security")
@@ -196,9 +192,10 @@ public class EmployeeWebControllerIntegrationTest {
 	public void formations_Test2() throws Exception {
 		mockMvc.perform(get("/employees/formations/"))
 				.andExpect(status().isUnauthorized());
+		verifyNoInteractions(service);
 	}
 	
-	@DisplayName("when employee HTTP status 200, then add employee to model and render view")
+	@DisplayName("when employee HTTP status 200, then add model and render view")
 	@Test
 	public void updateById_Test1() throws Exception {
 		when(service.fetchById(Long.valueOf(1))).thenReturn(employee);
@@ -216,6 +213,7 @@ public class EmployeeWebControllerIntegrationTest {
 		.andExpect(model().attribute("employee", Matchers.hasProperty("email", is(employee.getEmail()))))
 		.andExpect(model().attribute("employee", Matchers.hasProperty("addressData", is(employee.getAddressData()))))
 		.andExpect(model().attribute("employee", Matchers.hasProperty("userData", is(employee.getUserData()))));
+		verify(service, timeout(1)).fetchById(anyLong());
 	}
 	
 	@DisplayName("when not authorized user, then return status HTTP 401 - unauthorized, verifying security")
@@ -223,17 +221,142 @@ public class EmployeeWebControllerIntegrationTest {
 	public void updateById_Test2() throws Exception {
 		mockMvc.perform(get("/employees/updatings/{id}", Long.valueOf(1)))
 				.andExpect(status().isUnauthorized());
+		verifyNoInteractions(service);
 	}
 	
-	@DisplayName("")
+	@DisplayName("when employee HTTP status 200, then add model and render view")
 	@Test
 	public void showById_Test1() throws JsonProcessingException, Exception {
 		when(service.fetchById(anyLong())).thenReturn(employee);
 		mockMvc.perform(get("/employees/showings/{id}", Long.valueOf(1))
 				.with(user("TESTER")))
 				.andExpect(status().isOk())
+				.andExpect(view().name("detailsemployee"))
 				.andExpect(model().attributeExists("employee"))
-				.andExpect(view().name("detailsemployee"));
+				.andExpect(model().hasNoErrors())
+				.andExpect(model().attribute("employee", Matchers.hasProperty("firstName", is(employee.getFirstName()))))
+				.andExpect(model().attribute("employee", Matchers.hasProperty("lastName", is(employee.getLastName()))))
+				.andExpect(model().attribute("employee", Matchers.hasProperty("birthDate", is(employee.getBirthDate()))))
+				.andExpect(model().attribute("employee", Matchers.hasProperty("age", is(employee.getAge()))))
+				.andExpect(model().attribute("employee", Matchers.hasProperty("salary", is(employee.getSalary()))))
+				.andExpect(model().attribute("employee", Matchers.hasProperty("email", is(employee.getEmail()))))
+				.andExpect(model().attribute("employee", Matchers.hasProperty("addressData", is(employee.getAddressData()))))
+				.andExpect(model().attribute("employee", Matchers.hasProperty("userData", is(employee.getUserData()))));
+				verify(service, timeout(1)).fetchById(anyLong());
+	}
+	
+	@DisplayName("when not authorized user, then return status HTTP 401 - unauthorized, verifying security")
+	@Test
+	public void showById_Test2() throws Exception {
+		mockMvc.perform(get("/employees/showings/{id}", Long.valueOf(1)))
+				.andExpect(status().isUnauthorized());
+		verifyNoInteractions(service);
+	}
+	
+	@DisplayName("when HTTP status 200, then add to model 1 Employee==employee and render view 'listemployees'")
+	@Test
+	public void showAll_Test1() throws Exception {
+		when(service.fetchAll()).thenReturn(List.of(employee));
+		mockMvc.perform(get("/employees/showings/")
+				.with(user("TESTER")))
+		.andExpect(status().isOk())
+		.andExpect(view().name("listemployees"))
+		.andExpect(model().hasNoErrors())
+		.andExpect(model().attributeExists("employees"))
+		.andExpect(model().attribute("employees", Matchers.hasSize(1)))
+		.andExpect(model().attribute("employees", Matchers.hasItem(
+				allOf(
+						Matchers.hasProperty("firstName", is(employee.getFirstName())),
+						Matchers.hasProperty("lastName", is(employee.getLastName())),
+						Matchers.hasProperty("birthDate", is(employee.getBirthDate())),
+						Matchers.hasProperty("age", is(employee.getAge())),
+						Matchers.hasProperty("salary", is(employee.getSalary())),
+						Matchers.hasProperty("email", is(employee.getEmail())),
+						Matchers.hasProperty("addressData", is(employee.getAddressData())),
+						Matchers.hasProperty("userData", is(employee.getUserData()))
+						)
+				)));
+		verify(service, timeout(1)).fetchAll();
+	}
+	
+	@DisplayName("when not authorized user, then return status HTTP 401 - unauthorized, verifying security")
+	@Test
+	public void showAll_Test2() throws Exception {
+		mockMvc.perform(get("/employees/showings/"))
+				.andExpect(status().isUnauthorized());
+		verifyNoInteractions(service);
+	}
+	
+	@DisplayName("when HTTP 302, then render view, redirect and list employees size==0")
+	@Test
+	public void deleteById_Test1() throws Exception {
+		var employees = new ArrayList<Employee>();
+		employees.add(employee);
+		assertEquals(1, employees.size());
+		
+		doAnswer(new Answer<Employee>() {
+
+			@Override
+			public Employee answer(InvocationOnMock invocation) throws Throwable {
+				employees.remove(0);
+				return null;
+			}
+		}).when(service).deleteById(anyLong());
+		
+		mockMvc.perform(get("/employees/remove/{id}", Long.valueOf(1))
+				.with(user("TESTER")))
+		.andExpect(status().isFound())
+		.andExpect(redirectedUrl("/employees/showings"))
+		.andExpect(view().name("redirect:/employees/showings"));
+		
+		assertNotNull(employees);
+		assertEquals(0, employees.size());
+		verify(service, timeout(1)).deleteById(anyLong());
+	}
+
+	@DisplayName("when not authorized user, then return status HTTP 401, verifying security")
+	@Test
+	public void deleteById_Test2() throws Exception {
+		mockMvc.perform(get("/employees/remove/{id}", Long.valueOf(1)))
+				.andExpect(status().isUnauthorized());
+		verifyNoInteractions(service);
+	}
+
+	@DisplayName("when HTTP status 200, then add to model 1 Employee==employee and render view 'listemployees'")
+	@Test
+	public void showByLastname_Test1() throws Exception {
+		when(service.fetchByLastName(Mockito.anyString())).thenReturn(List.of(employee));
+		
+		mockMvc.perform(get("/employees/showings/lastname")
+				.param("lastname", "a".repeat(10))
+				.with(user("TESTER")))
+		.andExpect(status().isOk())
+		.andExpect(view().name("listemployees"))
+		.andExpect(model().hasNoErrors())
+		.andExpect(model().attributeExists("employees"))
+		.andExpect(model().attribute("employees", Matchers.hasSize(1)))
+		.andExpect(model().attribute("employees", hasItem(
+				allOf(
+						Matchers.hasProperty("firstName", is(employee.getFirstName())),
+						Matchers.hasProperty("lastName", is(employee.getLastName())),
+						Matchers.hasProperty("birthDate", is(employee.getBirthDate())),
+						Matchers.hasProperty("age", is(employee.getAge())),
+						Matchers.hasProperty("salary", is(employee.getSalary())),
+						Matchers.hasProperty("email", is(employee.getEmail())),
+						Matchers.hasProperty("addressData", is(employee.getAddressData())),
+						Matchers.hasProperty("userData", is(employee.getUserData()))
+						)
+				)));
+		verify(service, timeout(1)).fetchByLastName(Mockito.anyString());
+	}
+	
+	@DisplayName("when not authorized user, then return status HTTP 401, verifying security")
+	@Test
+	public void showByLastname_Test2() throws Exception {
+		mockMvc.perform(get("/employees/showings/lastname")
+				.param("lastname", "a".repeat(10)))
+				.andExpect(status().isUnauthorized());
+		verifyNoInteractions(service);
 	}
 	
 }
