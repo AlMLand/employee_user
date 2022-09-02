d4e5a507e728907a4caa499624454e242eacbd2f
Alex Morland <morlandalex@googlemail.com>
2022-09-02 23:53:36 +0200

    intellij: migrate the project to intellij

--- employee_user/target/test-classes/db/clean-up.sql
+++
@@ -1,6 +0,0 @@
-drop table if exists address CASCADE 
-drop table if exists email CASCADE 
-drop table if exists employees CASCADE 
-drop table if exists roles CASCADE 
-drop table if exists users CASCADE 
-drop table if exists users_roles CASCADE 
